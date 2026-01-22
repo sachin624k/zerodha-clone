32ab9e7e0e847b8b99a6236b407605b1d138b8d0
sachin624k <sachin77000k@gmail.com>
2026-01-22 21:53:22 +0530

    Updated Hero Section

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -5,32 +5,34 @@ function Hero() {
         <div className='container p-3 mt-4 mb-0'>
             <div className='row text-center'>
                 <img
-                    src='media/images/homeHero.png'
+                    src='media/images/landing.svg'
                     alt='Hero Image'
                     className='mb-4'
                     style={{
-                        width: "72%",
+                        width: "61%",
                         margin: "0 auto",
-                        maxWidth: "100%"  // responsive support
+                        marginTop: "20px",
+                        maxWidth: "100%"
                     }}
                 />
 
-                <h1 className='mt-3 mb-2 fs-4' style={{ color: "#424242" }}>
+                <h1 className='mt-5 mb-2 fs-2' style={{ color: "#424242" }}>
                     Invest in everything
                 </h1>
 
-                <p className='mb-4' style={{ color: "#424242" }}>
+                <p className='mb-4 fs-5' style={{ color: "#424242" }}>
                     Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.
                 </p>
 
                 <button
-                    className='p-1 btn fs-6 mb-2'
+                    className='p-1 btn fs-5 mb-2'
                     style={{
-                        width: "16%",
+                        width: "15%",
                         backgroundColor: "#387ed1",
                         color: "white",
-                        borderRadius: "2px",
-                        margin: "0 auto"
+                        borderRadius: "3px",
+                        margin: "0 auto",
+                        marginTop: "25px" 
                     }}
                 >
                     Sign up for free
