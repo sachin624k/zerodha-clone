f8c83472e5837d58ef204bf31338a4e483f558c3
sachin624k <sachin77000k@gmail.com>
2026-01-23 19:01:40 +0530

    Added Zerodha-style About section

--- frontend/src/landing_page/about/Hero.js
+++ frontend/src/landing_page/about/Hero.js
@@ -2,7 +2,37 @@ import React from 'react';
 
 function Hero() {
     return (
-        <h1>Hero</h1>
+        <div className='container'>
+            <div className='row p-5 mt-5 mb-5'>
+                <h1 className='text-center' style={{ fontSize: "26px", color: "#424242" }}>We pioneered the discount broking model in India.<br />
+                    Now, we are breaking ground with our technology.
+                </h1>
+            </div>
+            <div className='row p-5 mt-5 border-top text-muted'>
+                <div className='col-6 p-5' style={{lineHeight: "1.8", fontSize: "1.1em"}}>
+                    <p style={{marginLeft: "85px"}}>
+                        We kick-started operations on the 15th of August, 2010 with the goal of breaking all barriers that traders and investors face in India in terms of cost, support, and technology. We named the company Zerodha, a combination of Zero and "Rodha", the Sanskrit word for barrier.
+                    </p>
+                    <p style={{marginLeft: "85px"}}>
+                        Today, our disruptive pricing models and in-house technology have made us the biggest stock broker in India.
+                    </p>
+                    <p style={{marginLeft: "85px"}}>
+                        Over 1.6+ crore clients place billions of orders every year through our powerful ecosystem of investment platforms, contributing over 15% of all Indian retail trading volumes.
+                    </p>
+                </div>
+                <div className='col-6 p-5' style={{lineHeight: "1.8", fontSize: "1.1em"}}>
+                    <p style={{marginRight: "95px"}}>
+                        In addition, we run a number of popular open online educational and community initiatives to empower retail traders and investors.
+                    </p>
+                    <p style={{marginRight: "95px"}}>
+                        <a href='' style={{textDecoration: "none"}}>Rainmatter</a>, our fintech fund and incubator, has invested in several fintech startups with the goal of growing the Indian capital markets.
+                    </p>
+                    <p style={{marginRight: "95px"}}>
+                        And yet, we are always up to something new every day. Catch up on the latest updates on our <a href='' style={{textDecoration: "none"}}>blog</a> or see what the media is <a href='' style={{textDecoration: "none"}}>saying about us</a> or learn more about our business and product <a href='' style={{textDecoration: "none"}}>philosophies</a>.
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -2,7 +2,55 @@ import React from 'react';
 
 function Team() {
     return (
-        <h1>Team</h1>
+        <div className='container'>
+            <div className='row p-3 mt-0'>
+                <h1 className='text-center' style={{ fontSize: "26px", color: "#424242" }}>People
+                </h1>
+            </div>
+            <div className='row p-3 text-muted'>
+                <div className='col-5 p-3 text-center'>
+                    <img src="media/images/nithinKamath.jpg" style={{ marginLeft: "120px", borderRadius: "100%", width: "60%" }} />
+                    <h4 className='mt-4' style={{ marginLeft: "120px", fontSize: "20px", fontWeight: "400", color: "#424242" }}>Nithin Kamath</h4>
+                    <h6 className='mt-3' style={{ marginLeft: "120px", fontSize: "15px", fontWeight: "350", color: "#666666" }}>Founder, CEO</h6>
+                </div>
+                <div className='col-7 p-3' style={{ lineHeight: "1.8", fontSize: "1.1em" }}>
+                    <p>
+                        Nithin bootstrapped and founded Zerodha in 2010 to overcome the<br/> hurdles he faced during his decade long stint as a trader. Today,<br/> Zerodha has changed the landscape of the Indian broking industry.
+                    </p>
+                    <p>
+                        He is a member of the SEBI Secondary Market Advisory Committee<br/> (SMAC) and the Market Data Advisory Committee (MDAC).
+                    </p>
+                    <p>
+                        Playing basketball is his zen.
+                    </p>
+                    <p>
+                        Connect on <a href='https://nithinkamath.me/' style={{textDecoration: "none"}}>Homepage</a> / <a href='https://tradingqna.com/u/nithin/summary' style={{textDecoration: "none"}}>TradingQnA</a> / <a href='https://x.com/Nithin0dha' style={{textDecoration: "none"}}>Twitter</a>
+                    </p>
+                </div>
+            </div>
+
+            <div className='row p-3 text-muted'>
+                <div className='col-5 p-3 text-center'>
+                    <img src="media/images/sachin.jpeg" style={{ marginLeft: "120px", borderRadius: "90%", width: "60%", height: "80%" }} />
+                    <h4 className='mt-4' style={{ marginLeft: "120px", fontSize: "20px", fontWeight: "400", color: "#424242" }}>Sachin Kushwaha</h4>
+                    <h6 className='mt-3' style={{ marginLeft: "120px", fontSize: "15px", fontWeight: "350", color: "#666666" }}>Cloner</h6>
+                </div>
+                <div className='col-7 p-3' style={{ lineHeight: "1.8", fontSize: "1.1em" }}>
+                    <p>
+                        Sachin is a web developer currently cloning Zerodha’s platform to study<br /> its product architecture, design language, and UX philosophy. His work<br /> revolves around implementing responsive layouts, reusable components,<br/> and clean interaction patterns.
+                    </p>
+                    <p>
+                        Most days, he’s learning something new, breaking something old, and<br />trying to build things that make sense.
+                    </p>
+                    <p>
+                        Outside of code, he follows geopolitics and new ideas.
+                    </p>
+                    <p>
+                        Connect on <a href='https://x.com/sachin624k' style={{textDecoration: "none"}}>Twitter</a> / <a href='https://www.linkedin.com/in/sachin624k/' style={{textDecoration: "none"}}>Linkedin</a> / <a href='https://github.com/sachin624k' style={{textDecoration: "none"}}>GitHub</a>
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 }
 
