ae53bffcfbfeeb1939a6d6012f3fab8d2cff2d3a
sachin624k <sachin77000k@gmail.com>
2026-01-31 18:29:20 +0530

    Inserted data into DB

--- backend/model/PositionsModel.js
+++ backend/model/PositionsModel.js
@@ -2,6 +2,6 @@ const { model } = require("mongoose");
 
 const { PositionsSchema } = require('../schemas/PositionsSchema');
 
-const PositionsModel =  new model("holding", PositionsSchema);
+const PositionsModel =  new model("position", PositionsSchema);
 
 module.exports = { PositionsModel };
